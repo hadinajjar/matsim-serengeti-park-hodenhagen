d722f28a08ade6b72fdbbd91a6c0699455349e14
Hadi Najjar <hadi.najjar933@gmail.com>
2023-06-06 14:31:41 +0200

    created a simple link event handler

---
+++ src/main/java/org/matsim/analysis/LinkEventHandler.java
@@ -0,0 +1,32 @@
+package org.matsim.analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.LinkLeaveEvent;
+import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
+import org.matsim.api.core.v01.network.Link;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinkEventHandler implements LinkLeaveEventHandler {
+
+    private static final Id<Link> linkOfInterest = Id.createLinkId("7232382780000f");
+    private final Map<String, Integer> volumes = new HashMap<>();
+    @Override
+    public void handleEvent(LinkLeaveEvent event) {
+
+        if (event.getLinkId().equals(linkOfInterest)) {
+            String key = getKey(event.getTime());
+            volumes.merge(key, 1, Integer::sum);
+        }
+
+    }
+
+    private String getKey(double time) {
+        return Integer.toString((int) (time/3600));
+    }
+
+    public Map<String, Integer> getVolumes() {
+        return volumes;
+    }
+}
